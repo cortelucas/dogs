1a5a109d84db312aa20d1459074c0f92db15a350
Lucas Corte <corte1994.lc@gmail.com>
2021-11-23 10:18:32 -0300

    construção dos codes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { UserStorage } from './Context/UserContext'
 import Header from './Components/Header'
 import Footer from './Components/Footer'
 import Home from './Pages/Home/Home'
 import Login from './Pages/Login/Login'
 import User from './Pages/User/User'
+import { UserContext, UserStorage } from './Context/UserContext'
 import ProtectedRoute from './Components/Helper/ProtectedRoute'
 
 const App = () => {
+  const context = React.useContext(UserContext)
+  console.log(typeof context)
   return (
     <div>
       <BrowserRouter>
@@ -18,7 +20,7 @@ const App = () => {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="login/*" element={<Login />} />
-            <ProtectedRoute path="conta/*" element={<User />} />
+            <Route path="conta/*" element={<ProtectedRoute><User /></ProtectedRoute>} />
             {/* <Route path="*" element={<PageNotFound />} /> */}
           </Routes>
           <Footer />
