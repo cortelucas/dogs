9f7020c0861b442bf9da100ff6925e237a88d273
Lucas Corte <corte1994.lc@gmail.com>
2021-11-23 09:11:32 -0300

    paginas

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import React from 'react'
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Footer from './Components/Footer'
+import { UserStorage } from './Context/UserContext'
 import Header from './Components/Header'
+import Footer from './Components/Footer'
 import Home from './Pages/Home/Home'
 import Login from './Pages/Login/Login'
+import User from './Pages/User/User'
+import ProtectedRoute from './Components/Helper/ProtectedRoute'
 
-function App() {
+const App = () => {
   return (
     <div>
       <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-        <Footer />
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="login/*" element={<Login />} />
+            <ProtectedRoute path="conta/*" element={<User />} />
+            {/* <Route path="*" element={<PageNotFound />} /> */}
+          </Routes>
+          <Footer />
+        </UserStorage>
       </BrowserRouter>
     </div>
   )
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import { UserContext } from '../../Context/UserContext'
 
 const Home = () => {
+  const { login } = React.useContext(UserContext)
+  console.log(login)
   return (
     <div>
       Home
